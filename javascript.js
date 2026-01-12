7969a9e229eaaabf3987d48f515b9b7c8df4700e
Olivier DONEAU <odoneau@leni.fr>
2026-01-12 11:28:07 +0100

    feat: add "playRound" function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const randomValue = Math.floor(Math.random() * 3);
   return getChoice(randomValue);
@@ -16,6 +19,62 @@ function getChoice(value) {
 }
 
 function getHumanChoice() {
-  let userChoice = parseInt(prompt("Enter a value between 0 to 2:"));
+  let userChoice = parseInt(prompt("Enter a value between 0 to 2 (0: rock, 1: paper, 2: scissors):"));
   return getChoice(userChoice);
 }
+
+function playRound(humanChoice, computerChoice) {
+  console.log(`Human choice "${humanChoice}" vs computer choice "${computerChoice}"`);
+  switch (humanChoice) {
+    case "scissors":
+      switch (computerChoice) {
+        case "scissors":
+          console.log("Tie! No one wins.");
+          break;
+        case "paper":
+          console.log("You win! Scissors beats paper.");
+          humanScore++;
+          break;
+        default:
+          console.log("You lose! Rock beats scissors.");
+          computerScore++;
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerChoice) {
+        case "scissors":
+          console.log("You lose! Scissors beats paper.");
+          computerScore++;
+          break;
+        case "paper":
+          console.log("Tie! No one wins.");
+          break;
+        default:
+          console.log("You win! Paper beats rock.");
+          humanScore++;
+          break;
+      }
+      break;
+    default:
+      switch (computerChoice) {
+        case "scissors":
+          console.log("You win! Rock beats scissors.");
+          humanScore++;
+          break;
+        case "paper":
+          console.log("You lose! Paper beats rock.");
+          computerScore++;
+          break;
+        default:
+          console.log("Tie! No one wins.");
+          break;
+      }
+      break;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
